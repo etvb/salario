13a2602f5e4bbca115192c9b4db8df2306cc3504
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-09-17 23:10:11 -0500

    archivos base

---
+++ salario/src/salario/Salario.java
@@ -0,0 +1,23 @@
+/*Ejercicio 5: Un obrero necesita  calcular su salario semanal, el cual se
+ * obtienen de esta manera: Si trabaja 40 hrs o menos se le paga 16 pesos por
+ * hora, si trabaja mas de 40 hrs se le paga 16 pesos por las primeras 40 hrs y
+ * 20 por hora extra.
+*/
+package salario;
+
+import javax.swing.JOptionPane;
+
+public class Salario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+       
+        
+        
+        
+    }
+    
+}
